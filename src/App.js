f3b22581d1cb427b297f7cbbc863274953f618af
Shreya Sharma <shreya.sharma@nbgspl.com>
2022-01-27 20:07:38 +0530

    Add signup form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Login from './Components/Login/Login';
+import SignUp from './Components/SignUp/SignUp';
+
 
 
 function App() {
   return (
     <div className="App">
   <Login/>
+  <SignUp />
     </div>
   );
 }
